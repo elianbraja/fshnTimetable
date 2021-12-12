267688cf518ca803dac2c56b0ae6f5422f5d9e9a
elianbraja <elianbraja@yahoo.com>
2021-12-13 00:16:31 +0100

    app icon changed

--- src/services/Api/Api.js
+++ src/services/Api/Api.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 import { resolve } from './Resolver.js';
 
 export async function getCreator() {
-  return await resolve(axios.get('http://192.168.1.53:3000/creator').then(res => res.data));
+  return await resolve(axios.get('https://fshn-timetable.herokuapp.com/creator').then(res => res.data));
 }
 
 export async function getStudentTimetable(academic_year, group, subject) {
-  return await resolve(axios.get(`http://192.168.1.53:3000/timetable/student?academic_year=${academic_year}&group=${group}&subject=${subject}`).then(res => res.data));
+  return await resolve(axios.get(`https://fshn-timetable.herokuapp.com/timetable/student?academic_year=${academic_year}&group=${group}&subject=${subject}`).then(res => res.data));
 }
 
 export async function getPedagogTimetable(email) {
-  return await resolve(axios.get(`http://192.168.1.53:3000/timetable/pedagog?email=${email}`).then(res => res.data));
+  return await resolve(axios.get(`https://fshn-timetable.herokuapp.com/timetable/pedagog?email=${email}`).then(res => res.data));
 }
 
 export async function getSubjects() {
-  return await resolve(axios.get('http://192.168.1.53:3000/timetable/subjects').then(res => res.data));
+  return await resolve(axios.get('https://fshn-timetable.herokuapp.com/timetable/subjects').then(res => res.data));
 }
 
 export async function getProfessors() {
-  return await resolve(axios.get('http://192.168.1.53:3000/timetable/professors').then(res => res.data));
+  return await resolve(axios.get('https://fshn-timetable.herokuapp.com/timetable/professors').then(res => res.data));
 }
