5807a66b9da00523d55322a09c270b4c8dbbb969
elianbraja <elianbraja@yahoo.com>
2022-01-08 23:57:56 +0100

    add mail icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, AsyncStorage, Button } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage, Button, Linking } from 'react-native';
 import Timetable from './screens/Timetable'
 import Search from './screens/Search'
 import Header from './components/Header'
 import{ NativeRouter, Switch, Route } from 'react-router-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default function App({navigation}) {
 
   const Stack = createStackNavigator();
+
+  let infoPoint = (
+    <View style={{paddingRight: 20}}>
+      <FontAwesome name="info-circle" size={25} color="#eff2ed" onPress={() => Linking.openURL('mailto:elianbraja@gmail.com')}/>
+    </View>
+  )
 
     return (
       <View style={styles.container}>
@@ -28,6 +35,9 @@ export default function App({navigation}) {
                 headerTitleStyle: {
                   fontWeight: 'bold',
                 },
+                headerRight: () => (
+                  infoPoint
+                )
               }}
             />
             <Stack.Screen
@@ -42,7 +52,10 @@ export default function App({navigation}) {
                 headerTintColor: '#fff',
                 headerTitleStyle: {
                   fontWeight: 'bold',
-                }
+                },
+                headerRight: () => (
+                  infoPoint
+                )
               })}
             />
           </Stack.Navigator>
