28131ee53a79dff5f267920fc97d5df0d2637a66
Elian Braja <elian.braja@finetuning.it>
2023-01-30 14:51:44 +0100

    changed backend url

--- src/services/Api/Api.js
+++ src/services/Api/Api.js
@@ -2,29 +2,29 @@ import axios from 'axios';
 import { resolve } from './Resolver.js';
 
 export async function getCreator() {
-  return await resolve(axios.get('https://fshn-timetable.herokuapp.com/creator').then(res => res.data));
+  return await resolve(axios.get('https://timetable-fshn.fly.dev/creator').then(res => res.data));
 }
 
 export async function getStudentTimetable(academic_year, group, study_field) {
-  return await resolve(axios.get(`https://fshn-timetable.herokuapp.com/timetable/student?academic_year=${academic_year}&group=${group}&study_field=${study_field}`).then(res => res.data));
+  return await resolve(axios.get(`https://timetable-fshn.fly.dev/timetable/student?academic_year=${academic_year}&group=${group}&study_field=${study_field}`).then(res => res.data));
 }
 
 export async function getProfessorTimetable(email) {
-  return await resolve(axios.get(`https://fshn-timetable.herokuapp.com/timetable/professor?email=${email}`).then(res => res.data));
+  return await resolve(axios.get(`https://timetable-fshn.fly.dev/timetable/professor?email=${email}`).then(res => res.data));
 }
 
 export async function getStudyFields() {
-  return await resolve(axios.get('https://fshn-timetable.herokuapp.com/timetable/study_fields').then(res => res.data));
+  return await resolve(axios.get('https://timetable-fshn.fly.dev/timetable/study_fields').then(res => res.data));
 }
 
 export async function getProfessors() {
-  return await resolve(axios.get('https://fshn-timetable.herokuapp.com/timetable/professors').then(res => res.data));
+  return await resolve(axios.get('https://timetable-fshn.fly.dev/timetable/professors').then(res => res.data));
 }
 
 export async function getAcademicYears(study_field) {
-  return await resolve(axios.get(`https://fshn-timetable.herokuapp.com/timetable/academic_years?study_field=${study_field}`).then(res => res.data));
+  return await resolve(axios.get(`https://timetable-fshn.fly.dev/timetable/academic_years?study_field=${study_field}`).then(res => res.data));
 }
 
 export async function getGroups(study_field, academic_year) {
-  return await resolve(axios.get(`https://fshn-timetable.herokuapp.com/timetable/groups?study_field=${study_field}&academic_year=${academic_year}`).then(res => res.data));
+  return await resolve(axios.get(`https://timetable-fshn.fly.dev/timetable/groups?study_field=${study_field}&academic_year=${academic_year}`).then(res => res.data));
 }
